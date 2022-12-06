08da5a00c897f92d9bc268d15f7f18bc0d1d2276
Maxim Komoza <93898606+Komoza@users.noreply.github.com>
2022-12-06 16:12:36 +1000

    aside && main

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ===== typing animation =====
+let typed = new Typed('.typing', {
+    strings:['Software Engeener', 'Frontend developer', 'Smart man'],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
